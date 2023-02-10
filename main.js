6924ab85c442558942ab413061be434f764ec548
yoonseo <wownd8796@gmail.com>
2023-02-10 11:13:31 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+{
+  /* <script
+      src="https://code.jquery.com/jquery-3.6.3.js"
+      integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
+      crossorigin="anonymous"
+    ></script>
+    <script>
+      $.ajax({
+        method: "GET",
+        url: "https://dapi.kakao.com/v2/search/web",
+        data: { query: "영화 극한직업" },
+        headers: { Authorization: "KakaoAK ${REST_API_KEY}" },
+      }).done(function (msg) {
+        alert("Data Saved: " + msg);
+      });
+    </script> */
+}
